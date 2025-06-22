5cc78260a142e5712edcfac9faf7d6d1ed259943
Marta Sánchez <marta.msp97@gmail.com>
2025-06-22 20:42:32 +0200

    formulario del cliente hecho. Citas reservadas y guardadas en el localStorage correctamente

--- src/app/features/citas/cita-formulario-cliente/cita-formulario-cliente.component.ts
+++ src/app/features/citas/cita-formulario-cliente/cita-formulario-cliente.component.ts
@@ -1,10 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CitasService } from '../services/citas.service'
+import { Cita } from '../../../interfaces/cita.interface';
 
 @Component({
   selector: 'app-cita-formulario-cliente',
   templateUrl: './cita-formulario-cliente.component.html',
   styleUrls: ['./cita-formulario-cliente.component.css']
 })
-export class CitaFormularioClienteComponent {
+export class CitaFormularioClienteComponent implements OnInit {
+  cita: Partial<Cita> = {};
+  formGroup!: FormGroup;
 
+  constructor(
+    private router: Router,
+    private fb: FormBuilder
+  ) { }
+
+  ngOnInit(): void {
+    const state = history.state;
+    if (state && state.cita) {
+      this.cita = state.cita;
+    } else {
+      alert('No hay datos de cita.');
+      this.router.navigate(['/citas']);
+      return;
+    }
+    this.formGroup = this.fb.group({
+      cliente_nombre: ['', Validators.required],
+      cliente_telefono: ['', [Validators.required, Validators.pattern(/^\d{9}$/)]],
+      cliente_email: ['', Validators.email],
+      notas_cliente: ['']
+    });
+  }
+  onSubmit(): void {
+    if (this.formGroup.invalid || !this.cita) {
+      alert('Faltan datos por completar.');
+      return;
+    }
+
+    const citaCompleta: Cita = {
+      ...this.cita,
+      ...this.formGroup.value,
+      estado: 'pendiente',
+      notas_profesional: '',
+    };
+
+    // Guardar en localStorage
+    const citasGuardadas = JSON.parse(localStorage.getItem('citas') || '[]');
+    citasGuardadas.push(citaCompleta);
+    localStorage.setItem('citas', JSON.stringify(citasGuardadas));
+
+    // ✅ Confirmación y redirección
+    alert('¡Tu cita ha sido reservada correctamente!');
+    this.router.navigate(['/']);
+  }
 }
