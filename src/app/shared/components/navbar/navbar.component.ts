58e2ebb895d4ab8af69cde7b9dc6c99d8c5fd371
Marta Sánchez <marta.msp97@gmail.com>
2025-06-24 20:55:00 +0200

    logout

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AuthService } from 'src/app/features/auth/services/auth.service';
+import { Usuario } from 'src/app/interfaces/usuario';
 
 @Component({
   selector: 'app-navbar',
@@ -6,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
+
+  user: Usuario | null = null;
+
+  constructor(private authService: AuthService) { }
+
+  ngOnInit(): void {
+    this.user = this.authService.getUsuarioActivo() ?? null;
+  }
+
+  logout(): void {
+    this.authService.logout();
+    this.user = null;
+  }
 
 }
